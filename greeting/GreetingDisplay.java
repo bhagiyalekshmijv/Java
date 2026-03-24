f4121bb7f1093102872590f4f14303b82d2f9efc
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-03-25 00:46:48 +0530

    User Profile System Greeting display

---
+++ greeting/GreetingDisplay.java
@@ -0,0 +1,9 @@
+package greeting; // user-defined package
+
+public class GreetingDisplay {
+    
+    //Method to display greeting
+    public void showGreeting(String name) {
+        System.out.println("Welcome, " + name);
+    }
+}
