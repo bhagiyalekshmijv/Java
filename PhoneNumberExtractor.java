c95047ffb1a273f804608d97225a8f5e33bd670c
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-04-11 00:49:25 +0530

    Phone number extractor

---
+++ PhoneNumberExtractor.java
@@ -0,0 +1,31 @@
+import java.util.regex.*;
+
+public class PhoneNumberExtractor {
+    public static void main(String[] args) {
+        
+        String message = "Contact us at 9517538525 or office number 8975524133. Invaild: 1234";
+
+        findPhoneNumbers(message);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void findPhoneNumbers(String message) {
+
+        String regex = "\\b\\d{10}\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(message);
+
+        boolean found = false;
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+            found = true;
+        }
+
+        if (!found) {
+            System.out.println("No phone numbers found.");
+        }
+    }
+}
