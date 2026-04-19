45a9084a207a8bbc3cc1345a1a3f8982ac03ab78
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-04-19 13:00:06 +0530

    String format

---
+++ stringformat.java
@@ -0,0 +1,11 @@
+
+class MashupstackExample {
+    public static void main(String[] args) {
+    	
+        Double number = -4.2;
+        int num2 = 58;
+    	
+        System.out.println("Java from " + "Mashupstack.");
+        System.out.printf("Number = %f and %d ", number,num2);
+    }
+}
