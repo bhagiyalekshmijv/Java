54e260464eb0019c550e83716fc8f3a5d38deaa4
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-04-19 12:59:14 +0530

    Multiply floting point num

---
+++ MultiplyFloatingPointNums.java
@@ -0,0 +1,11 @@
+public class MultiplyFloatingPointNums {
+    public static void main(String[] args) {
+        
+        float num1 = 20.5f;
+        float num2 = 80.0f;
+
+        float product = num1 * num2;
+
+        System.out.println(num1 + " * " + num2 + " = " + product);
+    }
+}
