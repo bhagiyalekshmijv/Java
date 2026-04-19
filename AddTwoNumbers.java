fab5a5b3f6fe5cd57247a7d132d84db75936cbf5
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-04-19 12:59:24 +0530

    Add two num

---
+++ AddTwoNumbers.java
@@ -0,0 +1,11 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        
+        int num1 = 15;
+        int num2 = 40;
+        
+        int sum = num1 + num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + sum);
+    }
+}
