6818ac594570480044699063f00a9bac581f2b58
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-04-19 12:59:36 +0530

    ascii value

---
+++ AsciiValue.java
@@ -0,0 +1,12 @@
+public class AsciiValue {
+    public static void main(String[] args) {
+        
+        char ch = 'a';
+        int ascii = ch;
+
+        int castAscii = (int) ch;
+
+        System.out.println("Ascii value of " + ch + " is: " + ascii);
+        System.out.println("Ascii value of " + castAscii + " is: " + castAscii);
+    }
+}
