978ec73e0dc7891304d7077f7fb024e7e9cb7395
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-04-20 12:47:46 +0530

    Java exam 2 Multiplication

---
+++ JavaExam2MultiplicationTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class JavaExam2MultiplicationTable {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+
+        int num = input.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + (num * i));
+        }
+        input.close();
+    }
+}
