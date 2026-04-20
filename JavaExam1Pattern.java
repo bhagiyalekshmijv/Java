91bb31fa855ce74ef1897bfe5023bc59fee3e073
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-04-20 12:45:50 +0530

    Java Exam 1 Pattern

---
+++ JavaExam1Pattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class JavaExam1Pattern {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j =1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+}
